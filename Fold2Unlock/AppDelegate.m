d98246014dff088b75f9dd1e607063806a662759
Kyle Howells <freerunnering@gmail.com>
2012-05-23 21:35:11 +0100

    Setup the demo (only 6 lines of code).

--- Fold2Unlock/AppDelegate.m
+++ Fold2Unlock/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "KHFoldView.h"
 
 @implementation AppDelegate
 
@@ -23,6 +24,14 @@
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+
+    KHFoldView *foldView = [[[KHFoldView alloc] initWithFrame:_window.bounds] autorelease];
+    foldView.slices = 7;
+    foldView.viewImage = [UIImage imageNamed:@"screenshot.png"];
+    [self.window addSubview:foldView];
+
+    [UIApplication sharedApplication].statusBarHidden = YES;
+
     [self.window makeKeyAndVisible];
     return YES;
 }
