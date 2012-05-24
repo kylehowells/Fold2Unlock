0eada12145866e1ded01fa224770ade851bd65ab
Kyle Howells <freerunnering@gmail.com>
2012-05-24 15:18:48 +0100

    Added more comments (some unneeded) to explain everything.

--- Fold2Unlock/KHFoldView.m
+++ Fold2Unlock/KHFoldView.m
@@ -36,6 +36,7 @@
     if ((self = [super initWithFrame:frame])) {
         self.viewSlices = [NSMutableArray array];
 
+        // Track the touches
         UIPanGestureRecognizer *gestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(move:)];
         [self addGestureRecognizer:gestureRecognizer];
         [gestureRecognizer setMinimumNumberOfTouches:1];
@@ -109,6 +110,7 @@
 
 // Just render the images into UIImageViews on screen.
 -(void)updateSlicesFromImageArray:(NSArray*)array{
+    // Remove all the old views.
     for (int i = 0; i < [self.viewSlices count]; i++) {
         UIView *view = (UIView*)[self.viewSlices objectAtIndex:i];
         [view removeFromSuperview];
@@ -118,9 +120,16 @@
 
     for (int i = 0; i < [array count]; i++) {
         UIImageView *imageView = [[UIImageView alloc] initWithImage:[array objectAtIndex:i]];
-        imageView.layer.borderWidth = 0;
-        imageView.layer.anchorPoint = CGPointMake(0.5, 0.5);
         imageView.contentMode = UIViewContentModeScaleToFill;
+
+        // Do you want it to be the size of the image or fill the screen?
+        // Comment this out to be the size of the image
+        CGFloat width = self.bounds.size.width/[array count];
+        CGRect frame = imageView.frame;
+        frame.size.width = width;
+        frame.size.height = self.bounds.size.height;
+        imageView.frame = frame;
+
         [self addSubview:imageView];
         [self.viewSlices addObject:imageView];
         [imageView release];
@@ -135,12 +144,15 @@
     CGPoint translation = [gestureRecognizer translationInView:self];
     CGFloat completition = (translation.x / (self.bounds.size.width * 0.8)); // 0-1
 
+    // Stop anything weird from happening
     if (completition > 1) { completition = 1; }
     if (completition < 0 ) { completition = 0; }
 
     [self layoutSlicesForPercentage:completition];
 
+    // As it's just a demo finish up, you'll want to add some code to check whether or not to actually consider it as "unlocked" here.
     if (gestureRecognizer.state == UIGestureRecognizerStateEnded || gestureRecognizer.state == UIGestureRecognizerStateCancelled) {
+        // Animate it back into place.
         [UIView beginAnimations:nil context:nil];
         [self layoutSlicesForPercentage:0];
         [UIView commitAnimations];
@@ -149,16 +161,21 @@
 
 // percentage goes from 0 - 1
 -(void)layoutSlicesForPercentage:(CGFloat)percentage{
+    // For each view, rotate it
     for (int i = 0; i < [self.viewSlices count]; i++) {
-        CGFloat degrees = 90 * percentage;
+        CGFloat degrees = 90 * percentage; // How far along are we?
         if ((i % 2) == 0) {
-            degrees = 360-degrees;
+            degrees = 360-degrees; // rotate either forward or backwards.
         }
+        // I think in degrees but iOS uses radians so I need to convert it.
         CGFloat rotation = DEGREES_TO_RADIANS(degrees);
 
+        // Get the view
         UIView *view = [self.viewSlices objectAtIndex:i];
+        // Set the transform as a standard one..
         CATransform3D rotate = CATransform3DIdentity;
-        rotate.m34 = -1/500.0;
+        rotate.m34 = -1/500.0; // .. with a perspective
+        // ...and rotate it.
         rotate = CATransform3DRotate(rotate, rotation, 0.0f, 1.0f, 0.0f);
         view.layer.transform = rotate;
     }
@@ -167,12 +184,16 @@
     BOOL startPoint = NO;
     CGFloat currentX = self.bounds.size.width;
 
+    // Now run backwards through the array to lay the views out end to end (so it shrinks rather then flies apart)
     for (int i = 0; i < [viewSlices count]; i++) {
         UIView *view = [viewSlices objectAtIndex:([viewSlices count] - 1 - i)];
 
+        // This anchor point stuff is needed or there's a gap between the views forming as they rotate.
+        // We start at the the right side for the last view and switch each time.
         CGFloat x = (startPoint ? 0.0 : 1.0);
         view.layer.anchorPoint = CGPointMake(x, view.layer.anchorPoint.y);
 
+        // Set the view's position where we want it.
         if (view.layer.anchorPoint.x == 1) {
             view.layer.position = CGPointMake(currentX, view.layer.position.y);
         }
